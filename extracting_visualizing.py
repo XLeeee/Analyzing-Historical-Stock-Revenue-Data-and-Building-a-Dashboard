507aa0314f23dd2491acb4d2fc0f7930bd1ac03a
XLeeee <138759037+XLeeee@users.noreply.github.com>
2023-07-06 15:37:04 +0700

    Update extracting_visualizing.py

--- extracting_visualizing.py
+++ extracting_visualizing.py
@@ -1,3 +1,79 @@
 !pip install yfinance==0.1.67
 !mamba install bs4==4.10.0 -y
 !pip install nbformat==4.2.0
+
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+#----------------------Graph Function----------------------#
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+
+#----------------------Use yfinance to Extract Stock Data----------------------#
+#Using the Ticker function enter the ticker symbol of the stock we want to extract data on to create a ticker object. The stock is Tesla and its ticker symbol is TSLA.
+
+tesla = yf.Ticker("TSLA")
+
+#Using the ticker object and the function history extract stock information and save it in a dataframe named tesla_data. Set the period parameter to max so we get information for the maximum amount of time.
+tesla_data = tesla.history(period="max")
+
+#Use Webscraping to Extract Tesla Revenue Data
+#Use the requests library to download the webpage https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm Save the text of the response as a variable named html_data.
+
+url = " https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
+html_data  = requests.get(url).text
+
+#Parse the html data using beautiful_soup.
+
+beautiful_soup = BeautifulSoup(html_data, 'html5lib')
+
+#Using BeautifulSoup or the read_html function extract the table with Tesla Quarterly Revenue and store it into a dataframe named tesla_revenue. The dataframe should have columns Date and Revenue.
+
+tesla_revenue = pd.DataFrame(columns= ["Date","Revenue"])
+for row in beautiful_soup.find("tbody").find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text
+
+tesla_revenue = tesla_revenue.append({"Date":date,"Revenue": revenue},ignore_index = True)
+
+#Execute the following line to remove the comma and dollar sign from the Revenue column.
+
+tesla_revenue["Revenue"] = tesla_revenue['Revenue'].str.replace(',|\$',"")
+
+#Execute the following lines to remove an null or empty strings in the Revenue column.
+
+tesla_revenue.dropna(inplace=True)
+
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+
+#tesla_revenue.tail() #Display the last 5 row of the tesla_revenue dataframe using the tail function
+
+#-------------------Plot Tesla Stock Graph--------------------#
+
+make_graph(tesla_data, tesla_revenue, 'Tesla')
+
+
+
+
+
+
+
